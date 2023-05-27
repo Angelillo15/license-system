ab98ae00653047362c606b0592e97345bd099492
Angelillo15 <contact@angelillo15.es>
2023-05-27 13:21:53 +0200

    Change dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,23 +11,15 @@ repositories {
     maven("https://plugins.gradle.org/m2/")
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
-    implementation(project(":server"))
-    implementation("com.google.guava:guava:31.1-jre")
-    implementation("io.javalin:javalin:5.5.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.1")
-    implementation("org.slf4j:slf4j-simple:2.0.7")
-    implementation("org.projectlombok:lombok:1.18.28")
-}
-
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(11))
     }
 }
 
-
+dependencies {
+    implementation(project(":server"))
+}
 
 application {
     mainClass.set("es.angelillo15.license.LicenseServer")
@@ -54,13 +46,20 @@ allprojects {
 
     dependencies {
         testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
-        compileOnly("com.google.guava:guava:31.1-jre")
-        compileOnly("io.javalin:javalin:5.5.0")
-        compileOnly("com.fasterxml.jackson.core:jackson-databind:2.15.1")
-        compileOnly("org.slf4j:slf4j-simple:2.0.7")
-        compileOnly("org.projectlombok:lombok:1.18.28")
+        implementation("com.google.guava:guava:31.1-jre")
+        implementation("io.javalin:javalin:5.5.0")
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.15.1")
+        implementation("org.slf4j:slf4j-simple:2.0.7")
+        implementation("org.projectlombok:lombok:1.18.28")
         annotationProcessor("org.projectlombok:lombok:1.18.28")
-
+        implementation("org.tinylog:tinylog-api:2.6.2")
+        implementation("org.tinylog:tinylog-impl:2.6.2")
+        implementation("com.google.code.gson:gson:2.10.1")
+        implementation("com.github.Carleslc.Simple-YAML:Simple-Yaml:1.8.3")
+        implementation("com.github.Angelillo15:ConfigManager:1.4")
+        implementation("org.yaml:snakeyaml:1.33")
+        implementation("com.konghq:unirest-java:3.11.09")
+        implementation("io.javalin:javalin:5.3.2")
     }
 
     java {
