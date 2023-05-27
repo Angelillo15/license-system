478859d3db33d7f5a82c98c0fbcc6523a3ba508c
Angelillo15 <contact@angelillo15.es>
2023-05-27 03:37:49 +0200

    Added build logic and a basic javalin server

---
+++ server/src/main/java/es/angelillo15/license/LicenseServer.java
@@ -0,0 +1,44 @@
+package es.angelillo15.license;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+import io.javalin.json.JavalinJackson;
+import io.javalin.json.JsonMapper;
+import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LicenseServer {
+    @Getter
+    private static Javalin app;
+    @Getter
+    private static Logger logger = LoggerFactory.getLogger("LicenseServer");
+
+    public static void main(String[] args) {
+        app = Javalin.create(config -> {
+            ObjectMapper objectMapper = new ObjectMapper(); // customize as needed
+            JsonMapper jsonMapper = new JavalinJackson(objectMapper);
+            config.jsonMapper(jsonMapper);
+
+            config.spaRoot.addFile("/", "/client/index.html", Location.CLASSPATH);
+            config.staticFiles.add(staticFileConfig -> {
+                staticFileConfig.location = Location.CLASSPATH;
+                staticFileConfig.directory = "/client";
+                staticFileConfig.hostedPath = "/client";
+            });
+        });
+
+        // Prevent unhandled exceptions from taking down the web server
+        app.exception(Exception.class, (e, ctx) -> {
+            logger.error("Unhandled exception", e);
+            ctx.status(500);
+        });
+
+        app.start(5000);
+
+        logger.info("License server started");
+    }
+
+
+}
